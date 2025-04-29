4c78f97c47e84912361f791c3a060555dbfa2af3
devopspragmma01 <devopspragmma01@gmail.com>
2025-04-28 20:50:13 -0400

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send('Hello, AWS ECS!');
+  res.send('Hola equipo de curso de Devops, AWS ECS!');
 });
 
 app.listen(3000, () => {
